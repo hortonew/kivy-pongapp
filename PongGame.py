55adadd4431b86ed074d44b55a77ae2fb12f8908
Erik Horton <hortonew@gmail.com>
2018-01-15 14:22:09 -0700

    Fixed out of bounds issues

--- PongGame.py
+++ PongGame.py
@@ -11,22 +11,22 @@ class PongGame(Widget):
 	player1 = ObjectProperty(None)
 	player2 = ObjectProperty(None)
 
-	def serve_ball(self, vel=(30, 0)):
+	def serve_ball(self, vel=(10, 0)):
 		self.ball.center = self.center
 		self.ball.velocity = vel
 
 	def update(self, dt):
+		# Variables used to detect if collision happened
 		collided_p1, collided_p2 = False, False
-		collided_top_or_bot, scored = False, False
+		collided_top_or_bottom, scored = False, False
 
-		# Check bounds
+		# Get future value of x/y to determine if out of bounds
 		future_x = self.ball.x + self.ball.velocity_x
 		future_y = self.ball.y + self.ball.velocity_y
-		if (future_x <= 25 or (future_x >= self.width-25) or \
-			(future_y <= 0 or (future_y >= self.height))):
-			# Ball is going to be behind paddle
-			while (not collided_p1 and not collided_p2) and \
-				(not collided_top_or_bot and not scored):
+
+		# Ball outside bounds; increment by 1 until collision
+		if (future_x <= 25 or (future_x+self.ball.width >= self.width-25) or (future_y <= 0 or (future_y+self.ball.height >= self.height))):
+			while (not collided_p1 and not collided_p2 and not collided_top_or_bottom and not scored):
 
 				# Determine the velocity; calculate increment
 				if self.ball.velocity_x < 0:
@@ -39,6 +39,7 @@ class PongGame(Widget):
 				else:
 					y_inc = 1
 
+				# Move ball increment + collision detection
 				self.ball.x = self.ball.x + x_inc
 				self.ball.y = self.ball.y + y_inc
 				collided_p1 = self.player1.bounce_ball(self.ball)
@@ -47,9 +48,9 @@ class PongGame(Widget):
 				# bounce off top and bottom
 				if (self.ball.y < 0) or (self.ball.top > self.height):
 					self.ball.velocity_y *= -1
-					collided_top_or_bot = True
+					collided_top_or_bottom = True
 
-				# went off to a side to score point?
+				# Point scored; reset
 				if self.ball.x < self.x:
 					self.player2.score += 1
 					self.serve_ball(vel=(10,0))
@@ -58,20 +59,15 @@ class PongGame(Widget):
 					self.player1.score += 1
 					self.serve_ball(vel=(-10,0))
 					scored = True
-
-
 		else:
-			# call ball.move and other stuff
+			# Ball won't be outside bounds; move it
 			self.ball.move()
 			
-
 		# Basic AI for player 1
-		"""
 		if self.ball.y > self.player1.center_y:
 			self.player1.center_y += 3
 		if self.ball.y < self.player1.center_y:
 			self.player1.center_y -= 3
-		"""
 
 	# Handle drag
 	def on_touch_move(self, touch):
